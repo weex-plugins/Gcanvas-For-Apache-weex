42d889950b67ee37431b0a558370dba88dc54087
jerry.shb <jerry.shb@alibaba-inc.com>
2017-04-12 21:04:22 +0800

    + [ios] add header

---
+++ ios/Sources/WeexGcanvas.h
@@ -0,0 +1,16 @@
+//
+//  WeexGcanvas.h
+//  WeexGcanvas
+//
+//  Created by 齐山 on 17/4/12.
+//  Copyright © 2017年 weexplugin. All rights reserved.
+//
+
+#import "WXGCanvasModule.h"
+#import "WXGCanvasComponent.h"
+
+#ifndef WeexGcanvas_h
+#define WeexGcanvas_h
+
+
+#endif /* WeexGcanvas_h */
