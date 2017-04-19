1cd78fb16e84ab7b00a01dd25b111d96fffb9cbd
kaifeng.ly <kaifeng.ly@alibaba-inc.com>
2017-04-19 16:45:52 +0800

    gcanvas module component

---
+++ android/library/src/main/java/org/weex/plugin/weexplugingcanvas/WXGcanvasComponent.java
@@ -0,0 +1,81 @@
+package org.weex.plugin.weexplugingcanvas;
+
+import android.content.Context;
+
+import com.alibaba.weex.plugin.annotation.WeexComponent;
+import com.taobao.gcanvas.GCanvas;
+import com.taobao.weex.WXSDKInstance;
+import com.taobao.weex.annotation.Component;
+import com.taobao.weex.dom.WXDomObject;
+import com.taobao.weex.ui.ComponentCreator;
+import com.taobao.weex.ui.component.WXComponent;
+import com.taobao.weex.ui.component.WXVContainer;
+
+
+
+import java.lang.reflect.InvocationTargetException;
+
+@WeexComponent(names = {"gcanvas"})
+@Component(lazyload = false)
+public class WXGcanvasComponent extends WXComponent<WXGCanvasGLSurfaceView> {
+
+    public static class Creator implements ComponentCreator {
+        public WXComponent createInstance(WXSDKInstance instance, WXDomObject node, WXVContainer parent, boolean lazy) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+            return new WXGcanvasComponent(instance, node, parent, lazy);
+        }
+
+        public WXComponent createInstance(WXSDKInstance instance, WXDomObject node, WXVContainer parent) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+            return new WXGcanvasComponent(instance, node, parent);
+        }
+
+    }
+
+
+    @Deprecated
+    public WXGcanvasComponent(WXSDKInstance instance, WXDomObject dom, WXVContainer parent, String instanceId, boolean isLazy) {
+        this(instance, dom, parent, isLazy);
+    }
+
+    public WXGcanvasComponent(WXSDKInstance instance, WXDomObject node,
+                              WXVContainer parent, boolean lazy) {
+        super(instance, node, parent, lazy);
+    }
+
+
+    public WXGcanvasComponent(WXSDKInstance instance, WXDomObject node,
+                              WXVContainer parent) {
+        super(instance, node, parent);
+    }
+
+    @Override
+    protected WXGCanvasGLSurfaceView initComponentHostView(Context context) {
+
+        registerActivityStateListener();
+
+        WXGCanvasGLSurfaceView view = new WXGCanvasGLSurfaceView(context, null);
+
+        return view;
+    }
+
+
+    @Override
+    public void onActivityDestroy() {
+
+        if (GCanvas.fastCanvas != null) {
+
+            GCanvas.fastCanvas.onDestroy();
+            GCanvas.fastCanvas = null;
+        }
+
+        GcanvasModule.sRef = null;
+
+    }
+
+
+}
+
+
+
+
+
+
