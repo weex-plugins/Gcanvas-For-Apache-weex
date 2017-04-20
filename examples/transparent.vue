89cdf686c593dbdb5778d70e8dfb4d9c0b1fd680
jerry.shb <jerry.shb@alibaba-inc.com>
2017-04-20 14:25:18 +0800

    + [ios]update to transparent

---
+++ examples/transparent.vue
@@ -0,0 +1,41 @@
+<template>
+	<div ref="test">
+		<image class="img" style="width: 400px; height: 400px;" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACEAAAAhCAMAAABgOjJdAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAO1QTFRFhomSb3J8XGBrdnqDgYWNlJefV1tmfoGKU1diT1NfgISNgoWOYmZwio2Vi4+Wc3eAkJObZGhzUlZiaGt2U1djfYCKZGdyhYiQeHyFio6VgYWOVVllbHB6XWFsY2ZxaWx3jZCYXmFsfYCJcnZ/UVVham54V1tng4aPb3N8WV1pX2JtZWl0YWVvVlpmiYyUWVxohomRd3uEdHiBeX2GZ2t1hIeQVFhkf4OMbnJ8fYGKdnqEb3N9kJScY2dycnaAjpKZdHeBjpKajI+Yc3eBhYiRf4KLXmFtio6Wi4+XjZCZkJSbkZScjI+XlJifTlJe+rAZcwAAAZBJREFUeNp0lOeWgjAQhSdUBQRRbFjX7tpW3XUt23tL3v9xNigJIHh/zRk+5iR3ZgIkULWmwgv+A7VWDWWBR/nSt2gLUzIVbLH1lo8RRW1wEfqxPtCKUaKTMUhURuUhTIi6Q07l6LuAEFdpEld6tWNEx00CKKJ3jsR1xSHJcj4bB+LmkpxT49Yj8mqQUVBBciUk8ITepESJ+5AqY1+uwny5IjDPcADoN0BIwjLOsjKZOewtbi/9fvi338a44CetIai2H/cokPLj9tfEj2wVzD4rIWMlfpu+CXc5Pw4qh5X7AUw4gZIswUENGT8lAN1fMBVeAxIIxYR3Nin0imOe52emdxkyP8aY+0S9Y2d63cNoxvACRTaeI70slst+cjYCAnXmuuS1RKKmy1hKBX0hzaC3KOsxsmz2Ir2NzMcEbVCo+cf5oDOWOzNAucpxxoglnZlT1+KzXk4kymKwL4ttN+73dhHeuWV85zLL073Vw3v7MdDuE3a/JRrCmqwFQ2yV8gmvg/d+aPCMH0GLvB//AgwAo1MTpxsaxVoAAAAASUVORK5CYII="></image>
+		<gcanvas ref="canvas_holder" style="top: 0; width:750;height:750;position:absolute" ></gcanvas>
+	</div>
+</template>
+<script>
+	//	var gcanvas = weex.requireModule('weex-gcanvas');
+
+	var gcanvas=require('weex-gcanvas');
+
+
+	module.exports = {
+
+		created: function () {
+			console.log('created gcanvas');
+			gcanvas.disable();
+		},
+		mounted: function () {
+			var ref = this.$refs.canvas_holder.ref;
+			gcanvas.start(ref, function () {
+				var ctx = gcanvas.getContext('2d');
+
+				ctx.fillStyle = 'red';
+				ctx.fillRect(0, 0, 100, 100);
+
+				ctx.fillStyle = 'black';
+				ctx.fillRect(100, 100, 100, 100);
+				ctx.fillRect(25, 210, 700, 5);
+
+				ctx.arc(450, 200, 100, 0, Math.PI * 2, true);
+				ctx.fill();
+
+				var img = 'https://img.alicdn.com/tps/TB1TFNdKVXXXXbeaXXXXXXXXXXX-210-330.png';
+				ctx.drawImage(img, 100, 200, 210, 330);
+				//ctx.drawImage(img, 0,0,105,165, 100, 200, 210, 330);
+				ctx.render();
+			});
+		}
+	};
+</script>
