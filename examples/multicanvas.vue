6c3387d57ac91737387391f38926a3c39602d30f
韦青 <weixing.jwx@alibaba-inc.com>
2017-06-19 10:49:35 +0800

    [example] primitive & multi example

---
+++ examples/multicanvas.vue
@@ -0,0 +1,70 @@
+<template>
+	<div ref="test">
+		<gcanvas class="gcanvas1" ref="gcanvas1" style="width:750;height:300;background-color:rgba(255,0,0,0.3)"></gcanvas>
+		<gcanvas class="gcanvas2" ref="gcanvas2" style="width:750;height:300;background-color:rgba(0,255,0,0.3)"></gcanvas>
+	</div>
+</template>
+
+<style scoped>
+  .gcanvas1 {
+    position: absolute; 
+    top: 0; 
+    left: 0; 
+    right: 0; 
+    bottom: 300;
+  },
+  .gcanvas2 {
+    position: absolute; 
+    top: 350; 
+    left: 0; 
+    right: 0; 
+    bottom: 650;
+  }
+</style>
+
+<script>
+	var GCanvas = require('../js/src/gcanvas'); //调试使用
+
+	module.exports = {
+		created: function () {
+			GCanvas.disable();
+		},
+		mounted: function () {
+
+			var ref1 = this.$refs.gcanvas1;
+			GCanvas.start(ref1, function (gcanvas1) {
+				var ctx = gcanvas1.getContext('2d');
+				//rect
+				ctx.fillStyle = 'red';
+				ctx.fillRect(0, 0, 100, 100);
+
+				//rect
+				ctx.fillStyle = 'black';
+				ctx.fillRect(100, 100, 100, 100);
+				ctx.fillRect(25, 210, 700, 5);
+
+				// //circle
+				// ctx.arc(450, 200, 100, 0, Math.PI * 2, true);
+				// ctx.fill();				
+			});
+
+			var ref2 = this.$refs.gcanvas2;
+			GCanvas.start(ref2, function (gcanvas2) {
+				var ctx = gcanvas2.getContext('2d');
+				//rect
+				ctx.fillStyle = 'blue';
+				ctx.fillRect(100, 100, 100, 100);
+
+				//rect
+				ctx.fillStyle = 'yellow';
+				ctx.fillRect(100, 100, 100, 100);
+				ctx.fillRect(25, 210, 700, 5);
+
+				// //circle
+				// ctx.arc(450, 200, 100, 0, Math.PI * 2, true);
+				// ctx.fill();				
+			});
+		
+		}
+	};
+</script>
---
+++ examples/primitive.vue
@@ -0,0 +1,37 @@
+<template>
+	<div ref="test">
+		<gcanvas ref="canvas_holder" style="width:750;height:750;"></gcanvas>
+	</div>
+</template>
+<script>
+	// var gcanvas=require('weex-gcanvas'); //正式使用请用这个
+	var GCanvas=require('../js/src/gcanvas'); //调试使用
+
+
+	module.exports = {
+
+		created: function () {
+			console.log('created gcanvas');
+			GCanvas.disable();
+		},
+		mounted: function () {
+			var ref = this.$refs.canvas_holder;
+			GCanvas.start(ref, function (gcanvas) {
+				var ctx = gcanvas.getContext('2d');
+
+				//rect
+				ctx.fillStyle = 'red';
+				ctx.fillRect(0, 0, 100, 100);
+
+				//rect
+				ctx.fillStyle = 'black';
+				ctx.fillRect(100, 100, 100, 100);
+				ctx.fillRect(25, 210, 700, 5);
+
+				//circle
+				ctx.arc(450, 200, 100, 0, Math.PI * 2, true);
+				ctx.fill();				
+			});
+		}
+	};
+</script>
