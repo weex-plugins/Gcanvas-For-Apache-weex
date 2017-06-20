f7cb75c0d1ff5c7c81f8eb8ac2578b1344d322e6
韦青 <weixing.jwx@alibaba-inc.com>
2017-06-20 20:00:17 +0800

    [iOS] preload return jsTextureId

--- ios/Sources/WXGCanvasModule.m
+++ ios/Sources/WXGCanvasModule.m
@@ -193,7 +193,7 @@ WX_EXPORT_METHOD_SYNC(@selector(execGcanvaSyncCMD:args:));
         CGFloat width = CGImageGetWidth(cgimageRef);
         CGFloat height = CGImageGetHeight(cgimageRef);
         if(callback){
-            callback(@{@"width":@(width), @"height":@(height), @"id":@(imageCache.textureId)});
+            callback(@{@"width":@(width), @"height":@(height), @"id":@(imageCache.jsTextreId)});
         }
         
         if( !fromCache )
