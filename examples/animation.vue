033da24c1370954694166df8fb2372ced6ae07c4
韦青 <weixing.jwx@alibaba-inc.com>
2017-06-20 15:22:20 +0800

    [js] case animation

---
+++ examples/animation.vue
@@ -0,0 +1,33 @@
+<template>
+	<div ref="test">
+		<gcanvas ref="canvas_holder" style="top: 0; width:750;height:750;position:absolute" ></gcanvas>
+	</div>
+</template>
+<script>
+
+	// var gcanvas=require('weex-gcanvas'); //正式使用请用这个
+	var GCanvas=require('../js/src/gcanvas'); //调试使用
+
+
+	module.exports = {
+		created: function () {
+			console.log('created gcanvas');
+			GCanvas.disable();
+		},
+		mounted: function () {
+			var ref = this.$refs.canvas_holder;
+			GCanvas.start(ref, function (gcanvas) {
+				var ctx = gcanvas.getContext('2d');
+
+				var offsetX = 0;
+				setInterval(function(){
+					ctx.clearRect(0, 0, 750, 750);
+					ctx.fillStyle = 'red';
+					ctx.fillRect(offsetX, 0, 100, 100);
+
+					offsetX = (offsetX + 10 ) % 750;
+				}, 100);
+			});
+		}
+	};
+</script>
