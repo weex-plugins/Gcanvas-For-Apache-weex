dfb6ef5af2cb4b1107467147c95bc4fed3a05707
weixing.jwx <weixing.jwx@alibaba-inc.com>
2017-07-27 09:42:10 +0800

    comment

--- ios/Sources/WXGCanvasCallNative.m
+++ ios/Sources/WXGCanvasCallNative.m
@@ -1,33 +1,31 @@
-//
-//  WXGCanvasCallNative.m
-//  Pods
-//
-//  Created by weixing.jwx on 17/7/24.
-//
-//
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 
 #import "WXGCanvasCallNative.h"
 #import "WXGCanvasModule.h"
 
 @interface WXGCanvasCallNative ()
 
-@property (assign, nonatomic) dispatch_semaphore_t    semaphore;
-
-
 @end
 
 @implementation WXGCanvasCallNative
-
-- (instancetype)init
-{
-    self = [super init];
-    if (self)
-    {
-        _semaphore = dispatch_semaphore_create(0);
-    }
-    return self;
-}
-
 
 - (id)excuteCallNative:(NSDictionary *)dict
 {
