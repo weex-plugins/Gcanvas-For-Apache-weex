d48b008cf488e1b4a0ecd4ec3d52165c7ea2a96e
jwxbond <jwxbond@gmail.com>
2017-10-05 16:57:40 +0800

    更新

--- ios/Sources/WXBubbleComponent.m
+++ ios/Sources/WXBubbleComponent.m
@@ -19,6 +19,7 @@
     NSUInteger  _rowNum;
 }
 
+//WX_PlUGIN_EXPORT_COMPONENT(bubble,WXBubbleComponent, 1.0)
 WX_PlUGIN_EXPORT_COMPONENT(bubble,WXBubbleComponent)
 
 WX_EXPORT_METHOD(@selector(registerCallback:finished:bubbleClick:))
