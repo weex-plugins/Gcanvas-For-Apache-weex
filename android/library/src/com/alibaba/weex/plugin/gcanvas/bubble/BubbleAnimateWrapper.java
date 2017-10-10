ea793afa3eb90184dc405513d97b53ede572ef11
yuantong <yuantong.xyt@alibaba-inc.com>
2017-10-10 23:59:29 +0800

    fix a slide bug

--- android/library/src/com/alibaba/weex/plugin/gcanvas/bubble/BubbleAnimateWrapper.java
+++ android/library/src/com/alibaba/weex/plugin/gcanvas/bubble/BubbleAnimateWrapper.java
@@ -52,6 +52,8 @@ public class BubbleAnimateWrapper implements Comparable<BubbleAnimateWrapper> {
 
     private SpringSet mLastMoveSpring;
 
+    private SpringSet mLastBounce;
+
     BubbleAnimateWrapper(@NonNull View view, int index) {
         this.mView = view;
         this.mView.setPivotX(0);
@@ -237,23 +239,28 @@ public class BubbleAnimateWrapper implements Comparable<BubbleAnimateWrapper> {
     }
 
     void edgeBounce(int direction) {
+        if (null != mLastBounce && mLastBounce.isRunning()) {
+            mLastBounce.fastMove();
+        }
         if (direction == sDirectionLeft) {
             if (null != mView) {
                 SpringSet mBounceAnim = new SpringSet();
-                SpringAnimation springAnimation = SpringUtils.createSpring(mView, DynamicAnimation.TRANSLATION_X, mView.getTranslationX(), sMoveDamping, SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY);
-                springAnimation.setStartValue(mView.getTranslationX() - 100);
+                SpringAnimation springAnimation = SpringUtils.createSpring(mView, DynamicAnimation.X, mPosition.x, sMoveDamping, SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY);
+                springAnimation.setStartValue(mPosition.x - 100);
                 mBounceAnim.play(springAnimation);
                 mBounceAnim.addSpringSetListener(mEdgeLeftBounceListener);
                 mBounceAnim.start();
+                mLastBounce = mBounceAnim;
             }
         } else if (direction == sDirectionRight) {
             if (null != mView) {
                 SpringSet mBounceAnim = new SpringSet();
-                SpringAnimation springAnimation = SpringUtils.createSpring(mView, DynamicAnimation.TRANSLATION_X, mView.getTranslationX(), sMoveDamping, SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY);
-                springAnimation.setStartValue(mView.getTranslationX() + 100);
+                SpringAnimation springAnimation = SpringUtils.createSpring(mView, DynamicAnimation.X, mPosition.x, sMoveDamping, SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY);
+                springAnimation.setStartValue(mPosition.x + 100);
                 mBounceAnim.play(springAnimation);
                 mBounceAnim.addSpringSetListener(mEdgeRightBounceListener);
                 mBounceAnim.start();
+                mLastBounce = mBounceAnim;
             }
         }
     }
